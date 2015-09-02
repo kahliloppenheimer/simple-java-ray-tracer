567f0546e766e60ff1f3dad0043db8f97e0a24ca
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2015-09-02 14:19:46 -0400

    Added camera object

---
+++ src/edu/brandeis/cs/cosi155b/graphics/Camera3D.java
@@ -0,0 +1,25 @@
+package edu.brandeis.cs.cosi155b.graphics;
+
+import edu.brandeis.cs.cosi155b.shapes.Point3D;
+
+/**
+ * Created by kahliloppenheimer on 9/2/15.
+ */
+public class Camera3D {
+
+    private final Point3D location;
+    private final Point3D direction;
+
+    public Camera3D(Point3D location, Point3D direction) {
+        this.location = location;
+        this.direction = direction;
+    }
+
+    public Point3D getLocation() {
+        return location;
+    }
+
+    public Point3D getDirection() {
+        return direction;
+    }
+}
